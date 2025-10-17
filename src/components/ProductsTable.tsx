8c7ea7f66bedad3653835eddcc85e65b1a84e9c9
j8rge8a <jorgeluis_ochoa@outlook.com>
2025-10-17 03:26:44 -0600

    Version demo: Ajuste de resposividad en movil

--- src/components/ProductsTable.tsx
+++ src/components/ProductsTable.tsx
@@ -34,10 +34,10 @@ export function ProductsTable({
       <table className="w-full table-fixed text-sm sm:text-base border-collapse">
         <thead>
           <tr className="bg-allis-celeste">
-            <th className="px-2 py-1 text-left whitespace-nowrap w-[100px] sm:w-[150px]">Producto</th>
+            <th className="px-2 py-1 text-left whitespace-nowrap w-[100px] sm:w-[170px]">Producto</th>
             <th className="px-1 py-1 text-center whitespace-nowrap w-[60px] sm:w-[70px]">Cant.</th>
-            <th className="px-2 py-1 text-center whitespace-nowrap w-[75px] sm:w-[85px]">P/U</th>
-            <th className="px-2 py-1 text-center whitespace-nowrap w-[85px] sm:w-[95px]">P/T</th>
+            <th className="px-2 py-1 text-center whitespace-nowrap w-[75px] sm:w-[80px]">P/U</th>
+            <th className="px-2 py-1 text-center whitespace-nowrap w-[85px] sm:w-[90px]">P/T</th>
           </tr>
         </thead>
         <tbody>
